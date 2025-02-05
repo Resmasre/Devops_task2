61ab9bfbc0df6a7dbf69182a526af1011a435bc7
Resmasre <127740438+Resmasre@users.noreply.github.com>
2025-02-05 09:48:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u resmasre -p resma12345
-    docker tag test resmasre/task
-    docker push /task
+    docker tag test resmasre/task_2
+    docker push resmasre/task_2
     
